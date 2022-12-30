5f07737bf73a29d34a52fc11003102ffb494a980
Paulos Liu <paulos.liu@lytx.com>
2022-12-29 18:32:39 -0800

    Added code

---
+++ index.js
@@ -0,0 +1,70 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const settings = require('electron-settings');
+
+let mainWindow;
+let loggedIn = false;
+
+async function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    },
+    icon: './unnamed.png'
+  })
+
+  loggedIn = await getLoggedIn();
+  if (!loggedIn) {
+    mainWindow.loadURL("https://instagram.com")
+    setTimeout(() => {
+      console.log("you're locked out bro!")
+      setLoggedIn(true)
+      redirect()
+    }, 10000);
+  } else {
+    mainWindow.loadURL("https://instagram.com/direct")
+  }
+
+  mainWindow.webContents.on('did-navigate-in-page', (e, url) => {
+    if (!url.includes('direct') && loggedIn) {
+      mainWindow.loadURL("https://instagram.com/direct")
+    }
+  });
+}
+
+async function setLoggedIn(logged) {
+  await settings.set('loggedIn', logged);
+  loggedIn = logged;
+}
+
+async function getLoggedIn() {
+  return await settings.get('loggedIn');
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
